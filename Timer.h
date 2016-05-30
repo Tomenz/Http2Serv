d82dddf2f9a92f6a2398b9ed505d01a85b49b96e
Thomas <Thomas@fam-hauck.de>
2016-05-31 00:30:55 +0200

    Neue Timerfunktion

--- Timer.h
+++ Timer.h
@@ -11,12 +11,29 @@
 
 #pragma once
 
+#include <condition_variable>
+
 class Timer
 {
 public:
-    Timer(uint32_t tMilliSeconds, function<void(Timer*)> fTimeOut) : m_tMilliSeconds(tMilliSeconds), m_tpStart(chrono::system_clock::now()), m_fTimeOut(fTimeOut), m_bStop(false)
+    Timer(uint32_t tMilliSeconds, function<void(Timer*)> fTimeOut) : m_tMilliSeconds(tMilliSeconds), /*m_tpStart(chrono::system_clock::now()),*/ m_fTimeOut(fTimeOut)
     {
+        atomic_init(&m_bStop, false);
+
         m_thWaitThread = thread([&]() {
+
+            do
+            {
+                mutex mut;
+                unique_lock<mutex> lock(mut);
+                if (m_cv.wait_for(lock, chrono::milliseconds(m_tMilliSeconds)) == cv_status::timeout)
+                {
+                    if (m_fTimeOut != 0)
+                        m_fTimeOut(this);
+                    break;
+                }
+            } while (m_bStop == false);
+/*
             uint64_t  nDifMilliSec;
             do
             {
@@ -28,31 +45,36 @@ public:
             } while (nDifMilliSec < m_tMilliSeconds);
 
             if (m_bStop == false && m_fTimeOut != 0)
-                m_fTimeOut(this);
+                m_fTimeOut(this);*/
         });
     }
 
     virtual ~Timer()
     {
         m_bStop = true;
+        m_cv.notify_all();
         if (m_thWaitThread.joinable() == true)
             m_thWaitThread.join();
     }
 
     void Reset()
     {
-        atomic_init(&m_tpStart, chrono::system_clock::now());
+        //atomic_init(&m_tpStart, chrono::system_clock::now());
+        m_cv.notify_all();
     }
 
     void Stop()
     {
         m_bStop = true;
+        m_cv.notify_all();
     }
 
 private:
     uint32_t m_tMilliSeconds;
-    atomic<chrono::system_clock::time_point> m_tpStart;
+    //atomic<chrono::system_clock::time_point> m_tpStart;
     function<void(Timer*)> m_fTimeOut;
-    bool m_bStop;
+    atomic<bool> m_bStop;
     thread m_thWaitThread;
+
+    condition_variable m_cv;
 };
